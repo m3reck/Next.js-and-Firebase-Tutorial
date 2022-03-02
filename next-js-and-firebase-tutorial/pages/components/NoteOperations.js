e9b05d7aff8c023d3929a1bfa6f54f4a1e240715
Marek Masiak <masiakenator@gmail.com>
2022-03-02 17:00:46 +0100

    added sass

--- next-js-and-firebase-tutorial/pages/components/NoteOperations.js
+++ next-js-and-firebase-tutorial/pages/components/NoteOperations.js
@@ -4,7 +4,7 @@ import styles from "../../styles/Evernote.module.scss"
 export default function NoteOperations(){
     return(
      <>
-         <div className={styles.btnContainer}>
+         <div className={styles.btnContainer }>
              <button className={styles.button}>
                  Add a new Note
              </button>
