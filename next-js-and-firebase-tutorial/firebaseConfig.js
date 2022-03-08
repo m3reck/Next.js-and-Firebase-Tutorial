c14d8433f35e32fdf7136f909a80b5fa33744759
Marek Masiak <masiakenator@gmail.com>
2022-03-08 08:05:35 +0100

    mid commit

---
+++ next-js-and-firebase-tutorial/firebaseConfig.js
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAqrVKA0HGKlcd3AhLp9PsGx1E-uUFxd08",
+    authDomain: "evernoteclone-50f36.firebaseapp.com",
+    projectId: "evernoteclone-50f36",
+    storageBucket: "evernoteclone-50f36.appspot.com",
+    messagingSenderId: "1038208041546",
+    appId: "1:1038208041546:web:223afb0230bdfa38ce5cb3",
+    measurementId: "G-4HMBWVJWP1"
+};
+let analytics; let firestore;
+if (firebaseConfig?.projectId) {
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+
+    if (app.name && typeof window !== 'undefined') {
+        analytics = getAnalytics(app);
+    }
+
+    // Access Firebase services using shorthand notation
+    firestore = getFirestore();
+}
+
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const database = getFirestore(app)
--- next-js-and-firebase-tutorial/pages/components/NoteOperations.js
+++ next-js-and-firebase-tutorial/pages/components/NoteOperations.js
@@ -1,17 +1,81 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styles from "../../styles/Evernote.module.scss"
+import { app, database } from "../../firebaseConfig";
+import { collection, addDoc, getDocs } from "firebase/firestore"
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+
 
 export default function NoteOperations(){
+    const dbInstance = collection(database, "notes");
+
+    const [isInputVisible, setInputVisible] = useState(false);
+    const [noteTitle, setNoteTitle] = useState("");
+    const [noteDescription, setNoteDescription] = useState("");
+
+    //function that gets notes from database and returns it in a console
+    const getNotes = () => {
+        getDocs(dbInstance).then((data) => {
+            console.log(data);
+        })
+    }
+
+    useEffect(() => {
+        getNotes();
+    }, [])
+
+    //function that adds description
+    const addDescription = (value) => {
+        setNoteDescription(value)
+    }
+    //function that toggles the visibility of inputs
+    const inputToggle = () => {
+        setInputVisible(!isInputVisible)
+    }
+    //function that saves notes to the firebase db
+    const saveNote = () => {
+        addDoc(dbInstance, {
+            noteTitle: noteTitle,
+            noteDescription: noteDescription
+        })
+            //then clears the value box of inputs after fulfilling its promise
+            .then(() => {
+                setNoteTitle("");
+                setNoteDescription("");
+            })
+    }
     return(
      <>
          <div className={styles.btnContainer }>
-             <button className={styles.button}>
+             <button
+                 onClick={inputToggle}
+                 className={styles.button}>
                  Add a new Note
              </button>
          </div>
+         {isInputVisible ? (
          <div className={styles.inputContainer}>
-             <input placeholder="Enter the title.."/>
+             <input className={styles.input}
+                    placeholder="Enter the title.."
+                    onChange={(e) => setNoteTitle(e.target.value)}
+                    value={noteTitle}
+             />
+             <div className={styles.ReactQuill}>
+                 <ReactQuill
+                     onChange={addDescription}
+                     value={noteDescription}
+                 />
+             </div>
          </div>
+             ) : (
+                 <></>
+             )}
+         <button
+             onClick={saveNote}
+             className={styles.saveBtn}>
+             Save Note
+         </button>
      </>
     )
 }
